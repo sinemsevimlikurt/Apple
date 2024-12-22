877d32aaaeec3644fa129d4f1f9bd8b5f8b2d7da
alicantarim <alicantarim@hotmail.com>
2024-12-22 21:15:57 +0300

    .init branch

--- Apple/src/App.jsx
+++ Apple/src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1>Deneme</h1>
+      <h1>Deneme- Alican Branch</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
