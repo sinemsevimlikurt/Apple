a6f3a818d76e23592965738f70ee1597ada3d7b1
sinemsevimlikurt <sevimlikurtsinem@gmail.com>
2024-12-22 22:43:30 +0300

    header iskeleti tamamlandı

--- Apple/src/App.jsx
+++ Apple/src/App.jsx
@@ -1,16 +1,12 @@
 import "./App.css";
+import Header from "./Header/Header.jsx";
+import React from "react";
 
 function App() {
   return (
     <>
 
-      <h1>Deneme- Alican Branch</h1>
-
-
-      <h1>Deneme</h1>
-      <h2>Deneme</h2>
-      <h3>Denemelercekee</h3>
-      <h4>Deneme22</h4>
+    <Header/>
 
 
     </>
